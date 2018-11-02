d2ac1273c4a6686f3d7702d2d421c18d63705864
Daniel Platon <platon@adobe.com>
2018-11-02 17:52:28 +0200

    Setup our GraphQL server and write some sample queries

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,10 @@
-const mutations = {};
+const Mutation = {
+    createDog(parent, args, context, info) {
+        global.dogs = global.dogs || [];
+        const newDog = { name: args.name };
+        global.dogs.push(newDog);
+        return newDog;
+    }
+};
 
-module.exports = mutations;
+module.exports = Mutation;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,9 @@
-const Query = {};
+const Query = {
+    // the queries from the schema.graphql file have to be "implemented" here
+    dogs(parent, args, context, info) {
+        const dogs = global.dogs || [];
+        return dogs;
+    }
+};
 
 module.exports = Query;
