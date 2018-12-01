0d976f2e394eac17f02fa882a5c043638a4f4300
Daniel Platon <platon@adobe.com>
2018-12-02 00:12:06 +0200

    Permission Management (WIP)
    Render the permissions for each user.
    Handle the updates in the UI

--- frontend/components/Permissions.js
+++ frontend/components/Permissions.js
@@ -1,8 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
+import PropTypes from 'prop-types';
 
 import Table from "./styles/Table";
+import SickButton from './styles/SickButton';
+
 import Error from "./ErrorMessage";
 
 const ALL_USERS_QUERY = gql`
@@ -38,14 +41,14 @@ const Permissions = props => (
                                 <th>Name</th>
                                 <th>Email</th>
                                 {allPermissions.map(permission => (
-                                    <th>{permission}</th>
+                                    <th key={permission}>{permission}</th>
                                 ))}
                                 <th>👇</th>
                             </tr>
                         </thead>
                         <tbody>
                             {data.users.map(user => (
-                                <User user={user} />
+                                <UserPermissions key={user.id} user={user} />
                             ))}
                         </tbody>
                     </Table>
@@ -55,13 +58,48 @@ const Permissions = props => (
     </Query>
 );
 
-class User extends Component {
+class UserPermissions extends Component {
+    static propTypes = {
+        user: PropTypes.shape({
+            name:PropTypes.string.isRequired,
+            email:PropTypes.string.isRequired,
+            id:PropTypes.string.isRequired,
+            permissions:PropTypes.array.isRequired,
+        }).isRequired
+    }
+    state = {
+        permissions: this.props.user.permissions
+    }
+    handlePermissionChange = (e) => {
+        const checkbox = e.target;
+        // take a copy of the current permissions
+        let updatedPermissions = [...this.state.permissions];
+        if (checkbox.checked) {
+            updatedPermissions.push(checkbox.value);
+        } else {
+            updatedPermissions = updatedPermissions.filter( p => p !== checkbox.value);
+        }
+        this.setState({permissions:updatedPermissions});
+    }
     render() {
         const { user } = this.props;
         return (
             <tr>
                 <td>{user.name}</td>
                 <td>{user.email}</td>
+                {allPermissions.map(permission => (
+                    <td key={permission}>
+                        <label htmlFor={`${user.id}-permission-${permission}`}>
+                            <input type="checkbox" 
+                            checked={this.state.permissions.includes(permission)}
+                            value={permission}
+                            onChange={this.handlePermissionChange}/>
+                        </label>
+                    </td>
+                ))}
+                <td>
+                    <SickButton>Update</SickButton>
+                </td>
             </tr>
         );
     }
