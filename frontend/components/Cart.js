4d7bb5b0279239d484d62c1ebbcd466dc13a5b7c
Daniel Platon <platon@adobe.com>
2018-12-03 23:36:59 +0200

    Shopping cart - toggle the cart open / closed.
    Implement the local state changes using Apollo Client

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,23 +1,45 @@
-import React from 'react';
-import CartStyles from './styles/CartStyles';
-import SickButton from './styles/SickButton';
-import CloseButton from './styles/CloseButton';
-import Supreme from './styles/Supreme';
+import React from "react";
+import { Query, Mutation } from "react-apollo";
+import gql from "graphql-tag";
 
+import CartStyles from "./styles/CartStyles";
+import SickButton from "./styles/SickButton";
+import CloseButton from "./styles/CloseButton";
+import Supreme from "./styles/Supreme";
+
+export const LOCAL_STATE_QUERY = gql`
+    query {
+        cartOpen @client
+    }
+`;
+
+export const TOGGLE_CART_MUTATION = gql`
+    mutation {
+        toggleCart @client
+    }
+`;
 const Cart = () => {
-    return(
-        <CartStyles open>
-            <header>
-                <CloseButton title="close">&times;</CloseButton>
-                <Supreme>Your shopping cart</Supreme>
-                <p>You have __ items in cart</p>
-            </header>
-            <footer>
-                <p>$10.10</p>
-                <SickButton>Checkout</SickButton>
-            </footer>
-        </CartStyles>
-    )
-}
+    return (
+        <Mutation mutation={TOGGLE_CART_MUTATION}>
+            {toggleCart => (
+                <Query query={LOCAL_STATE_QUERY}>
+                    {({ data }) => (
+                        <CartStyles open={data.cartOpen}>
+                            <header>
+                                <CloseButton title="close" onClick={toggleCart}>&times;</CloseButton>
+                                <Supreme>Your shopping cart</Supreme>
+                                <p>You have __ items in cart</p>
+                            </header>
+                            <footer>
+                                <p>$10.10</p>
+                                <SickButton>Checkout</SickButton>
+                            </footer>
+                        </CartStyles>
+                    )}
+                </Query>
+            )}
+        </Mutation>
+    );
+};
 
 export default Cart;
---
+++ frontend/lib/withData.js
@@ -0,0 +1,48 @@
+import withApollo from "next-with-apollo";
+import ApolloClient from "apollo-boost";
+import { endpoint } from "../config";
+import { LOCAL_STATE_QUERY } from "../components/Cart";
+
+function createClient({ headers }) {
+    return new ApolloClient({
+        uri: process.env.NODE_ENV === "development" ? endpoint : endpoint,
+
+        request: operation => {
+            operation.setContext({
+                fetchOptions: {
+                    credentials: "include"
+                },
+                headers
+            });
+        },
+        // local state (data)
+        clientState: {
+            resolvers: {
+                Mutation: {
+                    toggleCart(_, variables, client) {
+                        //read the cartOpen value from the cache
+                        const cache = client.cache;
+                        const { cartOpen } = cache.readQuery({
+                            query: LOCAL_STATE_QUERY
+                        });
+                        console.log(cartOpen);
+
+                        //write the cart state to the opposite
+                        const data = {
+                            data: { cartOpen: !cartOpen }
+                        };
+
+                        cache.writeData(data);
+
+                        return data;
+                    }
+                }
+            },
+            defaults: {
+                cartOpen: true
+            }
+        }
+    });
+}
+
+export default withApollo(createClient);
