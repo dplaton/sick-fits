ea4ca1068977238524891fcda63b60c14dfba3de
Daniel Platon <platon@adobe.com>
2019-01-20 11:02:12 +0200

    Testing - add some sample unit tests

---
+++ frontend/__tests__/calcTotalPrice.test.js
@@ -0,0 +1,20 @@
+import calcTotalPrice from "../lib/calcTotalPrice";
+
+describe("The caclTotalPrice utility", () => {
+    it("should return the correct total for a properly defined cart", () => {
+        const cart = [
+            {
+                item: {
+                    price: 2000
+                },
+                quantity: 1
+            },
+            {
+                item: { price: 2000 },
+                quantity: 2
+            }
+        ];
+
+        expect(calcTotalPrice(cart)).toEqual(6000);
+    });
+});
---
+++ frontend/__tests__/formatMoney.test.js
@@ -0,0 +1,26 @@
+import formatMoney from '../lib/formatMoney';
+
+describe('The formatMoney utility', () => {
+    
+
+    it('should return the properly formatted amount', () => {
+        const VALUE_IN_CENTS=1234;    
+        const formattedMoneyValue=formatMoney(VALUE_IN_CENTS);
+
+        expect(formattedMoneyValue).toEqual('$12.34');
+    });
+
+    it('should leave out the decimals for fixed amount', () => {
+        const VALUE_IN_CENTS=1200;
+        const formattedMoneyValue = formatMoney(VALUE_IN_CENTS);
+
+        expect(formattedMoneyValue).toEqual('$12');
+    });
+
+    it('should work with a fraction of the dollar', () => {
+        const VALUE_IN_CENTS=12;
+        const formattedMoneyValue = formatMoney(VALUE_IN_CENTS);
+
+        expect(formattedMoneyValue).toEqual('$0.12'); 
+    })
+})
