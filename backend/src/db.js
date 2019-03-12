85f1f4f1575ae5923945bdb17adca20c7c8db517
Daniel Platon <platon@adobe.com>
2019-03-12 14:33:55 +0200

    Update the reference to the generated prisma.graphql file

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,7 +1,7 @@
 // connects to the remote Prisma DB
 const { Prisma } = require("prisma-binding");
 const db = new Prisma({
-    typeDefs: "src/generated/prisma.graphql",
+    typeDefs: "./src/generated/prisma.graphql",
     endpoint: process.env.PRISMA_ENDPOINT,
     secret: process.env.PRISMA_SECRET,
     debug: false
